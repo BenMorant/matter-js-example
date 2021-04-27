032e343ac0bd3ed0656b9a7c306b30be385670b1
Ben Mor </>
2021-04-27 12:21:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MatterExample from './Matter-example'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <MatterExample />
+      </div>
+    </div>
+  )
+}
+export default App
